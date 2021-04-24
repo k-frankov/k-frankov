2267fe2f5e6a2742c6eddcd7579b42aa9f29db42
KonFr <k.frankov@gmail.com>
2021-04-24 11:30:59 +0300

    Updates

---
+++ static/js/main.45aee1a3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpk-frankov"]=this["webpackJsonpk-frankov"]||[]).push([[0],{11:function(e,n,c){"use strict";c.r(n);var t=c(1),r=c.n(t),s=c(3),a=c.n(s),i=(c(8),c(9),c(0));var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("header",{className:"App-header",children:Object(i.jsx)("p",{children:"Hello!"})})})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,n,c){},9:function(e,n,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.45aee1a3.chunk.js.map
